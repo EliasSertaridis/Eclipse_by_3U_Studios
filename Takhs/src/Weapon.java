0eedcc3c9145ab5c897dc15aa2d9b319b494d57a
spybes01 <spybes@gmail.com>
2023-05-21 18:59:24 +0300

    work

---
+++ Takhs/src/Weapon.java
@@ -0,0 +1,48 @@
+public class Weapon extends Item {
+
+    int damage;
+    float scaling;
+    String[] damageType={"Slashing","Piercing","Bludgeoning"};
+    String[] weaponType={"Sword","Spear","Axe","Hammer"};
+
+
+    public Weapon(String name, String description,String[] weaponType, String[] damageType, int damage, float scaling) {
+        super(name, description);
+        this.damage= damage;
+        this.scaling= scaling;
+        this.damageType= damageType;
+        this.weaponType= weaponType;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public float getScaling() {
+        return scaling;
+    }
+
+    public void setScaling(float scaling) {
+        this.scaling = scaling;
+    }
+
+    public String[] getDamageType() {
+        return damageType;
+    }
+
+    public void setDamageType(String[] damageType) {
+        this.damageType = damageType;
+    }
+
+    public String[] getWeaponType() {
+        return weaponType;
+    }
+
+    public void setWeaponType(String[] weaponType) {
+        this.weaponType = weaponType;
+    }
+}
