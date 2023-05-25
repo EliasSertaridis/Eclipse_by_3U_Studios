7b2985dfb75d05269c5827f413c5bddca156c46a
spybe <spybes@gmail.com>
2023-05-25 18:49:12 +0300

    Added the inventory class and some methods needed for playerStatus. The playerStatus use case works until chooseEquipment,

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -4,16 +4,24 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        Inventory inventory=new Inventory();
 
         PlayableCharacter takhs = new PlayableCharacter("Takhs",12,20,9,40,80,7,3009,19,53.4,0.9);
        Item flask = new Item("takhs"," he is not here");
        Weapon sword= new Weapon("excalibur","The sword of king Arthur",13 ,10,0.3,Weapon.TypeOfDamage.Slashing,Weapon.TypeOfWeapon.Sword);
-       Armor head1= new Armor("kefali","its noice",13, Armor.TypeOfArmor.Head,1);
+        Weapon sword2= new Weapon("Zangetse","The sword of Ichigo",13 ,10,0.3,Weapon.TypeOfDamage.Slashing,Weapon.TypeOfWeapon.Sword);
+
+        Armor head1= new Armor("kefali","its noice",13, Armor.TypeOfArmor.Head,1);
        Armor head2= new Armor("KARANIO","its HUGE",13, Armor.TypeOfArmor.Head,1);
         Armor chest1= new Armor("sti8os","its noice",13, Armor.TypeOfArmor.Chest,1);
         Armor hands1= new Armor("xeria","its noice",13, Armor.TypeOfArmor.Hands,1);
         Armor legs1= new Armor("podia","its noice",13, Armor.TypeOfArmor.Legs,1);
         PlayerStatus playerStatus=new PlayerStatus();
+        playerStatus.setInventory(inventory);
+        playerStatus.inventory.addItemToInventory(head1);
+        playerStatus.inventory.addItemToInventory(head2);
+        playerStatus.inventory.addItemToInventory(sword2);
+        playerStatus.inventory.addItemToInventory(sword);
         playerStatus.setPlayer(takhs);
         CurrentEquipment eq1=new CurrentEquipment(head1,chest1,hands1,legs1,sword,sword);
         playerStatus.setEquipment(eq1);
--- Code/src/PlayerStatus.java
+++ Code/src/PlayerStatus.java
@@ -6,9 +6,17 @@ public class PlayerStatus {
 
     PlayableCharacter player;
     CurrentEquipment equipment;
-    // Inventory inventory;
+    Inventory inventory;
 
     public PlayerStatus() {
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
     }
 
     public Character getPlayer() {
@@ -28,121 +36,119 @@ public class PlayerStatus {
     }
 
     public void openPlayerStatus() {
-        int i=0;
-        boolean madeChoice=false;
-        Scanner scanner= new Scanner(System.in);
+        int i = 0;
+        boolean madeChoice = false;
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hp: "+player.getHp());
-        System.out.println("Level: "+player.getLevel());
+        System.out.println("Hp: " + player.getHp());
+        System.out.println("Level: " + player.getLevel());
         System.out.println("Equiped Armor: ");
         System.out.println("Head Piece: " + equipment.getHead().getName());
-        System.out.println("Chest Piece: "+ equipment.getChest().getName());
-        System.out.println("Hand Piece: "+equipment.getHands().getName());
-        System.out.println("Leg Piece: "+ equipment.getLegs().getName());
-        System.out.println("Right Weapon: "+ equipment.getRightWeapon().getName());
-        System.out.println("Left Weapon: "+equipment.getLeftWeapon().getName());
-        List<QuickItem> items=new ArrayList<>();
-        items=equipment.getCurrentQuickItems();
-        for(QuickItem item:items){
-            System.out.println("Quick Item Slot "+i+" "+ item.getName());
+        System.out.println("Chest Piece: " + equipment.getChest().getName());
+        System.out.println("Hand Piece: " + equipment.getHands().getName());
+        System.out.println("Leg Piece: " + equipment.getLegs().getName());
+        System.out.println("Right Weapon: " + equipment.getRightWeapon().getName());
+        System.out.println("Left Weapon: " + equipment.getLeftWeapon().getName());
+        List<QuickItem> items = new ArrayList<>();
+        items = equipment.getCurrentQuickItems();
+        for (QuickItem item : items) {
+            System.out.println("Quick Item Slot " + i + " " + item.getName());
             i++;
         }
-        while(i<5){
-            System.out.println("Quick Item Slot "+i+" Empty");
+        while (i < 5) {
+            System.out.println("Quick Item Slot " + i + " Empty");
             i++;
         }
         System.out.println("Enter which Field you want to change: ");
-        String input= scanner.nextLine();
-        while(!madeChoice) {
-            if (input.equals("Head")|| input.equals("Chest") || input.equals("Hand")||input.equals("Leg")|| input.equals("Right")|| input.equals("Left")) {
-                madeChoice=true;
+        String input = scanner.nextLine();
+        while (!madeChoice) {
+            if (input.equals("Head") || input.equals("Chest") || input.equals("Hand") || input.equals("Leg") || input.equals("Right") || input.equals("Left")) {
+                madeChoice = true;
                 chooseEquipment(input);
             } else {
                 System.out.println("You typed it wrong. Try again");
                 input = scanner.nextLine();
             }
-            }
-
         }
-    public void chooseEquipment(String choice){
-        if(choice.equals("Head")){
+
+    }
+
+    public void chooseEquipment(String choice) {
+        if (choice.equals("Head")) {
             chooseHead();
-        } else if(choice.equals("Hand")){
+        } else if (choice.equals("Hand")) {
             chooseHand();
-        }else if(choice.equals("Chest")){
+        } else if (choice.equals("Chest")) {
             chooseChest();
-        }else if(choice.equals("Leg")){
+        } else if (choice.equals("Leg")) {
             chooseLeg();
-        }else if(choice.equals("Right")){
+        } else if (choice.equals("Right")) {
             chooseRightWeapon();
-        }else if(choice.equals("Left")){
+        } else if (choice.equals("Left")) {
             chooseLeftWeapon();
-        }else
+        } else
             System.out.println("Nothing");
 
     }
+
     public void chooseHead() {
         int i = 1;
-        List<Armor> armor = equipment.getCurrentArmor();//prepei na bei to invetory
+        List<Armor> armor = inventory.getAllEquipmentOfType(Armor.TypeOfArmor.Head);
         System.out.println("All the Head Armor  of the player:");
         for (Armor head : armor) {
-            if (head.armorType == Armor.TypeOfArmor.Head) {
-                System.out.println(i+".  " + head.getName());
-                i++;
-            }
-        }
-    }
-    public void chooseChest(){
-        int i=1;
-        List<Armor> armor = equipment.getCurrentArmor();//prepei na bei to invetory
-        System.out.println("All the Chest Armor  of the player:");
-        for (Armor chest : armor) {
-            if (chest.armorType == Armor.TypeOfArmor.Chest) {
-                System.out.println(i+".  " + chest.getName());
-                i++;
-            }
-        }
-    }
-    public void chooseHand(){
-        int i=1;
-        List<Armor> armor = equipment.getCurrentArmor();//prepei na bei to invetory
-        System.out.println("All the Hand Armor  of the player:");
-        for (Armor hand : armor) {
-            if (hand.armorType == Armor.TypeOfArmor.Hands) {
-                System.out.println(i+".  " + hand.getName());
-                i++;
-            }
-        }
-    }
-    public void chooseLeg(){
-        int i=1;
-        List<Armor> armor = equipment.getCurrentArmor();//prepei na bei to invetory
-        System.out.println("All the Leg Armor  of the player:");
-        for (Armor leg : armor) {
-            if (leg.armorType == Armor.TypeOfArmor.Legs) {
-                System.out.println(i+".  " + leg.getName());
-                i++;
-            }
-        }
-    }
-    public void chooseRightWeapon(){
-        int i=1;
-        List<Weapon> weapons = equipment.getCurrentWeapons();//prepei na bei to invetory
-        System.out.println("All the Weapons  of the player:");
-        for (Weapon rightWeapon : weapons) {
-                System.out.println(i+".  " + rightWeapon.getName());
-                i++;
-        }
-    }
-    public void chooseLeftWeapon(){
-        int i=1;
-        List<Weapon> weapons = equipment.getCurrentWeapons();//prepei na bei to invetory
-        System.out.println("All the Weapons  of the player:");
-        for (Weapon leftWeapon : weapons) {
-            System.out.println(i+".  " + leftWeapon.getName());
+            System.out.println(i + ".  " + head.getName());
             i++;
         }
     }
+
+    public void chooseChest() {
+        int i = 1;
+        List<Armor> armor = inventory.getAllEquipmentOfType(Armor.TypeOfArmor.Chest);
+        System.out.println("All the Chest Armor  of the player:");
+        for (Armor chest : armor) {
+            System.out.println(i + ".  " + chest.getName());
+            i++;
+        }
     }
 
+    public void chooseHand() {
+        int i = 1;
+        List<Armor> armor = inventory.getAllEquipmentOfType(Armor.TypeOfArmor.Hands);
+        System.out.println("All the Hand Armor  of the player:");
+        for (Armor hand : armor) {
+            System.out.println(i + ".  " + hand.getName());
+            i++;
+        }
+    }
+
+    public void chooseLeg() {
+        int i = 1;
+        List<Armor> armor = inventory.getAllEquipmentOfType(Armor.TypeOfArmor.Legs);
+        System.out.println("All the Leg Armor  of the player:");
+        for (Armor leg : armor) {
+            System.out.println(i + ".  " + leg.getName());
+            i++;
+        }
+    }
+
+    public void chooseRightWeapon() {
+        int i = 1;
+        List<Weapon> equipment = inventory.getAllEquipmentOfType();
+        System.out.println("All the Weapons of the player:");
+        for (Equipment weapon : equipment) {
+            System.out.println(i + ".  " + weapon.getName());
+            i++;
+        }
+    }
+
+    public void chooseLeftWeapon() {
+        int i = 1;
+        List<Weapon> equipment = inventory.getAllEquipmentOfType();
+        System.out.println("All the Weapons of the player:");
+        for (Equipment weapon : equipment) {
+            System.out.println(i + ".  " + weapon.getName());
+            i++;
+        }
+    }
+}
 
