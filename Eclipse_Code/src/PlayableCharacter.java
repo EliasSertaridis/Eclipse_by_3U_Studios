19219e0bc6813415ce36b3cac1700dd9388d5b6e
EliasSertaridis <elias.serta@gmail.com>
2023-06-05 02:05:50 +0300

    made the setBaseStats method

--- Eclipse_Code/src/PlayableCharacter.java
+++ Eclipse_Code/src/PlayableCharacter.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PlayableCharacter extends Character{
     private int money;
@@ -94,5 +91,127 @@ public class PlayableCharacter extends Character{
 
     public void setDefenceMod(){
         this.defenceModifier = currentEquipment.getTotalDefense();
+    }
+
+    public void setBaseStats(PlayableCharacter pc){
+        boolean setStats = false;
+        int str=0;
+        int dex=0;
+        int vit=0;
+        int intel=0;
+        int wis=0;
+        int count=0;
+        System.out.println("You have 55 points to distribute between the following 5 stats:");
+        System.out.println("1. Strength: affects your equipload and the ability to use some weapons");
+        System.out.println("2. Dexterity: affects your speed");
+        System.out.println("3. Vitality: affects your resistance to status effects");
+        System.out.println("4. Intelligence: affects your ability to use spells that scale with this stat");
+        System.out.println("5. Wisdom: affects your ability to use spells that scale with this stat");
+        System.out.println("To add a point first write 'add' and then the number associated with the stat as presented above.");
+        System.out.println("To remove a point first write 'remove' and then the number associated with the stat as presented above.");
+        System.out.println("Whenever you feel satisfied with your stats, type 'done'.");
+        System.out.println("If you wish to see how you have distributed your points type 'show'.");
+        Scanner scanner = new Scanner(System.in);
+        while (!setStats){
+            String choice = scanner.nextLine();
+            if (choice=="add"){
+                int stat = scanner.nextInt();
+                switch (stat){
+                    case 1:
+                        str = str + 1;
+                        count++;
+                        pc.setStrength(str);
+                        break;
+                    case 2:
+                        dex = dex + 1;
+                        count++;
+                        pc.setDexterity(dex);
+                        break;
+                    case 3:
+                        vit = vit + 1;
+                        count++;
+                        pc.setVitality(vit);
+                        break;
+                    case 4:
+                        intel = intel + 1;
+                        count++;
+                        pc.setIntelligence(intel);
+                        break;
+                    case 5:
+                        wis = wis + 1;
+                        count++;
+                        pc.setWisdom(wis);
+                        break;
+                    default:
+                        System.out.println("You typed a false number. please try again, and make sure to type 'add' first.");
+                }
+            } else if (choice=="remove") {
+                int stat = scanner.nextInt();
+                switch (stat){
+                    case 1:
+                        str = str - 1;
+                        count--;
+                        pc.setStrength(str);
+                        break;
+                    case 2:
+                        dex = dex - 1;
+                        count--;
+                        pc.setDexterity(dex);
+                        break;
+                    case 3:
+                        vit = vit - 1;
+                        count--;
+                        pc.setVitality(vit);
+                        break;
+                    case 4:
+                        intel = intel - 1;
+                        count--;
+                        pc.setIntelligence(intel);
+                        break;
+                    case 5:
+                        wis = wis - 1;
+                        count--;
+                        pc.setWisdom(wis);
+                        break;
+                    default:
+                        System.out.println("You typed a false number. please try again, and make sure to type 'remove' first.");
+                }
+            } else if (choice=="done") {
+                if (count<55){
+                    System.out.println("You have not distributed all the available points. Do you wish to end this process?");
+                    System.out.println("For 'Yes' type 1, for 'No' type 2.");
+                    int yer = scanner.nextInt();
+                    if (yer==1) setStats = true;
+                    else if (yer==2) {
+                        break;
+                    } else {
+                        System.out.println("The input you provided is incorrect. Please try again.");
+                    }
+                } else {
+                    setStats = true;
+                }
+            } else if (choice=="show") {
+                System.out.println("Your current stats are: ");
+                System.out.println("Strength: " + pc.getStrength());
+                System.out.println("Dexterity: " + pc.getDexterity());
+                System.out.println("Vitality: " + pc.getVitality());
+                System.out.println("Intelligence: " + pc.getIntelligence());
+                System.out.println("Wisdom: " + pc.getWisdom());
+            } else {
+                System.out.println("The input you provided is incorrect. Please try again.");
+            }
+            if (count==55){
+                System.out.println("You have distributed all the available points. Your current stats are: ");
+                System.out.println("Strength: " + pc.getStrength());
+                System.out.println("Dexterity: " + pc.getDexterity());
+                System.out.println("Vitality: " + pc.getVitality());
+                System.out.println("Intelligence: " + pc.getIntelligence());
+                System.out.println("Wisdom: " + pc.getWisdom());
+                System.out.println("If you are satisfied with your current stats, type 'done'.");
+                String in = scanner.nextLine();
+                if (in=="done") setStats = true;
+                else System.out.println("The input you provided is incorrect. Please try again.");
+            }
+        }
     }
 }
